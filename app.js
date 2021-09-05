ca1edb6f4378639665263f5b3f7dfa655a51fad6
Puja Kundu <pujakundu797@gmail.com>
2021-09-05 18:19:43 +0600

    done

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const API_KEY = 'c862b1bec09836c1b2bafce5470c9055';
 const loadData = () => {
     const searchField = document.getElementById('search-feild');
     const searchValue = searchField.value;
-    console.log(searchValue);
+
     searchField.value = '';
     const API = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${API_KEY}&units=metric`;
     fetch(API)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayData(data));
 };
 const searchBtn = document.getElementById('button-search');
 searchBtn.addEventListener('click', loadData);
@@ -18,5 +18,13 @@ const updateText = (id, text) => {
 };
 
 const displayData = (data) => {
-    updateText('city-name', `${data.name}`)
+    updateText('city-name', data.name);
+    updateText('temparature', data.main.temp + '°C');
+    updateText('feels-like', 'Feels like ' + data.main.feels_like + '°C');
+    updateText('condition', data.weather[0].main);
+    const weatherIcon = document.getElementById('weather-icon');
+    const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIcon.setAttribute('src', iconUrl);
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.classList.add('results');
 }
